662505f7d6bc9b85eb8f89e8c079282a06be6650
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-07-01 13:40:02 +0100

    building-homepage

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const imageSlider = document.querySelectorAll('.header-slider ul img');
+const prevButton = document.querySelector('.control_prev');
+const nextButton = document.querySelector('.control_next');
+
+let n = 0;
+
+function changeSlide() {
+    for (let i = 0; i < imageSlider.length; i++) {
+        imageSlider[i].style.display = 'none';
+    }
+    imageSlider[n].style.display = 'block';
+};
+
+changeSlide();
+
+prevButton.addEventListener('click', (e) => {
+    if (n > 0) {
+        n--;
+    } else {
+        n = imageSlider.length - 1;
+    }
+    changeSlide();
+});
+
+nextButton.addEventListener('click', (e) => {
+    if (n < imageSlider.length - 1) {
+        n++;
+    } else {
+        n = 0;
+    }
+    changeSlide();
+})
