2832d99511e07e190b071cef44305423d8ed3c15
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-07-02 13:52:11 +0100

    adding-more-pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
+let navBar = document.getElementById("navbar");
+
+let navigationBar = () => {
+    return (navBar.innerHTML = navbarData
+      .map((x) => {
+        let { id, name, price, desc, img } = x;
+        let search = basket.find((x) => x.id === id) || [];
+        return `
+              <div id=product-id-${id} class="item">
+                  <img width="220" src=${img} alt="">
+                  <div class="details">
+                      <h3>${name}</h3>
+                      <p>${desc}</p>
+                      <div class="price-quantity">
+                          <h2>$ ${price}</h2>
+                          <div class="buttons">
+                              <i onclick=decrement(${id}) class="bi bi-dash-lg"></i>
+                              <div id=${id} class="quantity">
+                              ${search.item === undefined ? 0 : search.item}
+                              </div>
+                              <i onclick=increment(${id}) class="bi bi-plus-lg"></i>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              `;
+      })
+      .join(""));
+  };
+  
+  navigationBar();
+
+
+
 const imageSlider = document.querySelectorAll('.header-slider ul img');
 const prevButton = document.querySelector('.control_prev');
 const nextButton = document.querySelector('.control_next');
